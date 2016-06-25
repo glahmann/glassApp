bd7f1799ef764fd71f673774a0e3e74eb6be761d
Alex Merk <Alex Merk>
2016-06-25 11:34:03 -0700

    Add Glass Class

---
+++ src/Glass.java
@@ -0,0 +1,10 @@
+
+public class Glass {
+	private double length;
+	private double width;
+	
+	public Glass(double length, double width){
+		this.length = length + .75;
+		this.width = width + .75;
+	}
+}
